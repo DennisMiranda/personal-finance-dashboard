520ddddb5d9b01c6476d7c38048df14ae0259cb2
vienheno <herrera.victor@outlook.com>
2025-03-21 20:02:37 -0500

    commit para revisar otra rama

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -2,3 +2,9 @@ import { TransactionsTable } from './components/table';
 
 new TransactionsTable('#table-expenses', 'Gasto');
 new TransactionsTable('#table-incomes', 'Ingreso');
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   pageTransactions.addEventListener('click', function () {
+//     window.location.href = '../pages/transactions.html';
+//   });
+// });
